aac5f490e1e769e85fd157c25edbd19494fb1c97
KISHORERAJA28 <kishoreraaja@gmail.com>
2026-02-22 00:36:57 +0530

    Create Smart String Reducer.py
    Thhis is a  function that reduces a string by sequentially removing pairs of adjacent, case-insensitive matching characters
    
    Examples: 
    ● reduce_string("aBbA") returns "" 
     "aBbA" -> remove 'B' and 'b' -> becomes "aA" 
     "aA" -> remove 'a' and 'A' -> becomes ""

---
+++ Smart String Reducer.py
@@ -0,0 +1,12 @@
+def reduced_string(s:str):
+    stack=[]
+    for char in s:
+        if stack and stack[-1].lower()==char.lower():
+            stack.pop()
+        else:
+            stack.append(char)
+    return "" .join(stack)
+
+s=input("ENTER THHE STRING:")
+result=reduced_string(s)
+print(f"input:{s}and the result:{result}")
