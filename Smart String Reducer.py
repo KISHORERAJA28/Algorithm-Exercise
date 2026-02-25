fd8284275e627e21fc6f8a7165a4abce873efc8a
KISHORERAJA28 <kishoreraaja@gmail.com>
2026-02-25 22:51:13 +0530

    Update Smart String Reducer.py

--- Smart String Reducer.py
+++ Smart String Reducer.py
@@ -7,6 +7,7 @@ def reduced_string(s:str):
             stack.append(char)
     return "" .join(stack)
 
+print("Smart String Reducer")
 s=input("ENTER THHE STRING:")
 result=reduced_string(s)
 print(f"input:{s}and the result:{result}")
