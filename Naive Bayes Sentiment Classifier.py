4ae081c029a509aeee55e12bb7b9bb3dfb539130
KISHORERAJA28 <kishoreraaja@gmail.com>
2026-02-28 22:33:04 +0530

    Update Naive Bayes Sentiment Classifier.py

--- Naive Bayes Sentiment Classifier.py
+++ Naive Bayes Sentiment Classifier.py
@@ -5,7 +5,12 @@ def preprocess(text):
     text=re.sub(r'[^\w\s]','',text.lower())
     return text.split()
 
-
+class naivebayesclassifier:
+    def __init__(self):
+        self.vocab=set()
+        self.w_c={'positive':{},'negative':{}}
+        self.t_w={'positive':0,'negative':0}
+        self.prior={'positive':0,'negative':0}
 
 
 
